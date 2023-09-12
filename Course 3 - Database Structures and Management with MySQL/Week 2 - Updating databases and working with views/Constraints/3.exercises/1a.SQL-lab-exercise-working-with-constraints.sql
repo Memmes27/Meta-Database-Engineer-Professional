bcae31965120adcfd3d07ba3ac2128c06f85b84f
Mohammed Abdin <63331353+Memmes27@users.noreply.github.com>
2023-09-11 18:10:48 -0700

    Added Course files

---
+++ Course 3 - Database Structures and Management with MySQL/Week 2 - Updating databases and working with views/Constraints/3.exercises/1a.SQL-lab-exercise-working-with-constraints.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE Mangata_Gallo;
+USE Mangata_Gallo;
+
+#Task1
+CREATE TABLE Clients (ClientID INT NOT NULL PRIMARY KEY, FullName VARCHAR(100) NOT NULL,
+PhoneNumber INT NOT NULL UNIQUE);
+SHOW COLUMNS from Clients;
+#Task2
+CREATE TABLE Items (ItemID INT NOT NULL PRIMARY KEY, ItemName VARCHAR(100) NOT NULL,
+Price DECIMAL(5,2) NOT NULL);
+Show Columns from Items;
+#Task3
+CREATE TABLE ORDERS (
+OrderID INT NOT NULL PRIMARY KEY, 
+ClientID INT NOT NULL, ItemID INT NOT NULL,
+Quantity INT NOT NULL CHECK(Quantity <=3),
+Cost DECIMAL(6,2) NOT NULL,
+FOREIGN KEY (ClientID) REFERENCES Clients (ClientID),
+FOREIGN KEY (ItemID) REFERENCES Items (ItemID));
+
+Show Columns from Orders;
